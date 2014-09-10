8d451db52eb8d72548f1353e58f29c11f1c882fb
Administrador <Administrador@Notebook>
2014-09-09 23:00:43 -0300

    Implementa el proxy

--- RedesTarea1/src/NetworkAdmin.java
+++ RedesTarea1/src/NetworkAdmin.java
@@ -1,18 +1,31 @@
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 
 public class NetworkAdmin {
     public static void main(String[] args) throws Exception {
         // SOLICITUD: Parámetros
         String HTTPversion = "HTTP/1.1";
-        String route = "/principal";
-        String domain = "www.bps.gub.uy";
-        String connectionType = "keep-alive";
+        String route = "/";
+        String domain = "whatismyipaddress.com";
         String userAgent = "User-Agent: Mozilla/5.0";
         String language = "Accept-Language: es";
         int port = 80;
-
+        String proxyDomain;
+        int proxyPort;
+        boolean behindProxy = false;                                            // Sólo proxy público (sin autenticación)
+        String connectionType;
+        if (behindProxy) {
+            connectionType = "Proxy-Connection: keep-alive";
+            proxyDomain = "95.128.246.10";
+            proxyPort = 80;
+        } else {
+            connectionType = "keep-alive";
+            proxyDomain = domain;
+            proxyPort = port;
+        }
+        
         // SOLICITUD: Confección
         String humanRequest =
             "GET " + route + " " + HTTPversion + "\r\n" +
@@ -24,9 +37,9 @@ public class NetworkAdmin {
         byte[] machineRequest = humanRequest.getBytes();
         
         // SOCKET: Creación y envío
-        Socket socket = new Socket(domain, port);                               // Se está creando y conectando al socket de una. No se si es legal o quieren que hagamos el Socket primero y lo conectemos después.
+        Socket socket = new Socket();
+        socket.connect(new InetSocketAddress(proxyDomain, proxyPort));
         InputStream in = socket.getInputStream();
-        
         OutputStream out = socket.getOutputStream();
         out.write(machineRequest);
         
@@ -37,14 +50,15 @@ public class NetworkAdmin {
 
         int cumulativeRead = 0;
         int instanceRead = 0;
-        while (instanceRead != -1) {
+        while (instanceRead != -1 &&
+               instanceRead + 1 != cumulativeRead) {
             instanceRead = in.read(machineAnswer, cumulativeRead,
                                    buff - 1 - cumulativeRead);                  // El problema de la demora radica en el tamaño del buffer, al menos eso creo (aunque ahora lo retesteo y me anda rápido... fíjense qué tal les anda a ustedes).
             cumulativeRead += 1 + instanceRead;
         }
         
         // RESPUESTA: Interpretación
-        humanAnswer = new String(machineAnswer);
+        humanAnswer = new String(machineAnswer);                                // Podría no haber problemas con los chunks, a no ser que justo parta una tira que nos interese al medio (como un href o un mail). Hay que ver cómo solucionarlo.
         System.out.println("DATA RECEIVED:\n\n" + humanAnswer);
 
         // SOCKET: Cerrar
