af068930aa9d67d510abb1b4fc7d20feaaf6ef8b
Administrador <Administrador@Notebook>
2014-09-11 13:21:14 -0300

    Formateo de la salida con saltos de línea y observación sobre chunks

--- RedesTarea1/src/NetworkAdmin.java
+++ RedesTarea1/src/NetworkAdmin.java
@@ -7,16 +7,18 @@ import java.net.Socket;
 
 public class NetworkAdmin {
     public static void main(String[] args) throws Exception {
-        // SOLICITUD: Parámetros
+        // SOLICITUD: Encabezado HTTP
         String HTTPversion = "HTTP/1.1";
         String route = "/";
         String domain = "whatismyipaddress.com";
         String userAgent = "User-Agent: Mozilla/5.0";
         String language = "Accept-Language: es";
+        
+        // SOLICITUD: Conectividad
         int port = 80;
-        String proxyDomain;
-        int proxyPort;
         boolean behindProxy = false;                                            // Sólo proxy público (sin autenticación)
+        int proxyPort;
+        String proxyDomain;
         String connectionType;
         if (behindProxy) {
             connectionType = "Proxy-Connection: keep-alive";
@@ -46,18 +48,29 @@ public class NetworkAdmin {
         out.write(machineRequest);
         
         // SOCKET: Recepción
-        BufferedReader inBuff = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        BufferedReader inBuff = new BufferedReader(new InputStreamReader(in));
         String inputLine;
         StringBuffer response = new StringBuffer();
 
         while ((inputLine = inBuff.readLine()) != null) {
-                response.append(inputLine);
+                response.append(inputLine + "\n");
         }
         inBuff.close();
         
         // RESPUESTA: Interpretación
-        System.out.println("DATA RECEIVED:\n\n" + response);
+        System.out.println("DATA RECEIVED (CHUNKED):\n\n" + response);
 
+        // RESPUESTA: Unificación de chunks                                     // Hay que detectar los chunks cuando existen
+                                                                                // (leyendo el encabezado "Transfer-Encoding: chunked")
+                                                                                // y reunificar los trozos por si alguna numeración de bytes
+                                                                                // corta algún dato que nos interese.
+                                                                                //
+                                                                                // La dinámica para unificar sería leer el primer dato
+                                                                                // hexadecimal que indica la cantidad de caracteres a leer
+                                                                                // (hay que ver si cuenta los \n y verificar con un ejemplo
+                                                                                // lo que afirmo) y volver a leer el hexadecimal siguiente para
+                                                                                // repetir el procedimiento.
+        
         // SOCKET: Cerrar
         socket.close();
     }
