485b008830338dd5c7b5511538a107ef2bd79e4a
Diego Barreiro <diego.barreiro@internetbrands.com>
2014-09-13 14:57:30 -0300

    Class Parser (schema)

---
+++ RedesTarea1/src/redbot/Parser.java
@@ -0,0 +1,38 @@
+package redbot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+/**
+ *
+ * @author d
+ */
+public class Parser
+{
+    public Parser(String text)
+    {
+        // TODO
+    }
+
+    public List<String> urls()
+    {
+        // TODO
+        return new ArrayList();
+    }
+
+    public List<String> emails()
+    {
+        // TODO
+        return new ArrayList();
+    }
+
+    public Map<String, String> stats()
+    {
+        // TODO
+        return new HashMap();
+    }
+
+}
