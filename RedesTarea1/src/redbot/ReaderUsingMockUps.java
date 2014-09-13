808cd1b668183bee7ec9de8273c633df68d97b56
Diego Barreiro <diego.barreiro@internetbrands.com>
2014-09-13 16:32:28 -0300

    Add some functionality of MockUps reader (get)

--- RedesTarea1/src/redbot/ReaderUsingMockUps.java
+++ RedesTarea1/src/redbot/ReaderUsingMockUps.java
@@ -1,5 +1,6 @@
 package redbot;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -12,25 +13,57 @@ public class ReaderUsingMockUps implements Reader
     @Override
     public String get(String url) throws Exception
     {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Map<String, String> pages = new HashMap();
+        
+        String page;
+
+        /**
+         * Mails: 2 (one with "mailto:", one lose)
+         * URLs: 2
+         * Hiding: no
+         * Others: single and double quotes
+         */
+        page = "Hola\n"
+             + "esto es una página con un par de mails y algún link\n"
+             + "por ejemplo <a href='link1'>link1</a>\n"
+             + "o <a href=\"link2\">link2</a>\n"
+             + "y pa mails no sé... uno con mailto <a href='mailto:cuisdy@gmail.com'>mi mail</a>\n"
+             + "y otro suelto cuisdy@gmail.com'\n"
+             + "(en el próximo capítulo, mails medianamente escondidos)'\n";
+        pages.put("page1", page);
+
+        /**
+         * Mails: 
+         * URLs: 
+         * Hiding: 
+         * Others: 
+         */
+        page = "";
+        pages.put("page2", page);
+        
+        page = "La página que pediste no la conozco, te devuelvo"
+             + " cualquier cosa, sin mails ni urls ni nada, ta?";
+        pages.put("", page); // default
+
+        return pages.containsKey(url) ? pages.get(url) : pages.get("");
     }
 
     @Override
     public String post(String url) throws Exception
     {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return get(url);
     }
 
     @Override
     public String head(String url) throws Exception
     {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public Map<String, String> headers()
     {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet.");
     }
     
 }
